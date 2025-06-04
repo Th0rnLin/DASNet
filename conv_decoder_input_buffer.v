57c863906dfa7bf298f208faf28133dc35bd5acc
Thorn Lin <th0rnlin1412@gmail.com>
2025-06-05 03:36:48 +0800

    _

---
+++ conv_decoder_input_buffer.v
@@ -0,0 +1,20 @@
+`timescale 1ns/ 1ps
+
+module conv_decoder_input_buffer(
+    input  wire                clk,
+    input  wire                rst,
+    input  wire                start,
+    input  wire signed [17: 0] input_pixel,
+    output reg  signed [17: 0] x,
+    output reg                 ready
+);
+    always@(posedge clk) begin
+        if(rst) begin
+            x    <=0;
+            ready<=0;
+        end else if(start) begin
+            x    <=input_pixel;
+            ready<=1;
+        end
+    end
+endmodule
